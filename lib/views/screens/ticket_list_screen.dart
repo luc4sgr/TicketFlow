aaf23f4a7bf05ec31fbce17d887a647907ebfdc8
luc4sgr <23.lucasdoliveira@gmail.com>
2025-01-31 16:00:38 -0300

    refactor TicketListScreen to use TicketViewModel for loading and displaying tickets

--- lib/views/screens/ticket_list_screen.dart
+++ lib/views/screens/ticket_list_screen.dart
@@ -1,24 +1,49 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:provider/provider.dart';
+import '../../view_models/ticket_view_model.dart';
 
-class TicketListScreen extends StatelessWidget {
+class TicketListScreen extends StatefulWidget {
   const TicketListScreen({super.key});
+
+  @override
+  State<TicketListScreen> createState() => _TicketListScreenState();
+}
+
+class _TicketListScreenState extends State<TicketListScreen> {
+  @override
+  void initState() {
+    super.initState();
+    //TODO:verificar
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      Provider.of<TicketViewModel>(context, listen: false).loadTickets();
+    });
+  }
+
   @override
   Widget build(BuildContext contex) {
+    final viewModel = Provider.of<TicketViewModel>(context);
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Lista de Tickets'),
       ),
-      body: ListView.builder(
-        itemCount: 5,
-        itemBuilder: (context, index) {
-          return ListTile(
-            title: Text('Ticket ${index + 1}'),
-            subtitle: const Text('Ticket description'),
-            onTap: () => context.go('/tickets/$index'),
-          );
-        },
-      ),
+      body: viewModel.isLoading
+          ? const Center(
+              child: CircularProgressIndicator(),
+            )
+          : ListView.builder(
+              itemCount: viewModel.tickets.length,
+              itemBuilder: (context, index) {
+                final ticket = viewModel.tickets[index];
+                return ListTile(
+                  title: Text(ticket.title),
+                  subtitle: Text(ticket.description),
+                  trailing: Text(ticket.status),
+                  onTap: () => context.go('/ticket/${ticket.id}'),
+                );
+              },
+            ),
     );
   }
 }
