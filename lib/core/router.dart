ab67fa4c02e63b8d51ad9d1fbd5fb49725177a8c
luc4sgr <23.lucasdoliveira@gmail.com>
2025-01-31 16:37:13 -0300

    update router to change initial location and enhance ticket details navigation with custom transition

--- lib/core/router.dart
+++ lib/core/router.dart
@@ -1,21 +1,27 @@
+import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:ticket_flow/views/screens/home_screen.dart';
 import 'package:ticket_flow/views/screens/ticket_details_screen.dart';
 import 'package:ticket_flow/views/screens/ticket_list_screen.dart';
 
-final GoRouter router = GoRouter(initialLocation: '/', routes: [
+final GoRouter router = GoRouter(initialLocation: '/tickets', routes: [
   GoRoute(
     path: '/',
     builder: (context, state) => const HomeScreen(),
   ),
   GoRoute(
-    path: 'tickets',
+    path: '/tickets',
     builder: (context, state) => const TicketListScreen(),
   ),
   GoRoute(
-      path: '/tickets/id',
-      builder: (context, state) {
+      path: '/ticket/:id',
+      pageBuilder: (context, state) {
         final ticketId = state.pathParameters['id'];
-        return TicketDetailsScreen(ticketId: ticketId!);
+        return CustomTransitionPage(
+            child: TicketDetailsScreen(ticketId: ticketId!),
+            transitionsBuilder:
+                (context, animation, secondaryAnimation, child) {
+              return FadeTransition(opacity: animation, child: child);
+            });
       }),
 ]);
