46657877851e37b2be65f87f0d248397459ea99f
luc4sgr <23.lucasdoliveira@gmail.com>
2025-01-31 16:37:21 -0300

    implement caching for ticket data using SharedPreferences in TicketService

--- lib/services/ticket_service.dart
+++ lib/services/ticket_service.dart
@@ -1,14 +1,27 @@
 import 'dart:convert';
 import 'package:flutter/services.dart';
-import 'package:ticket_flow/models/ticket.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import '../models/ticket.dart';
 
 class TicketService {
-  Future<List<Ticket>> fetchTicket() async {
-    await Future.delayed(const Duration(seconds: 2));
-    //Carrega Json local como mock
+  Future<List<Ticket>> fetchTickets() async {
+    final prefs = await SharedPreferences.getInstance();
+    final cachedData = prefs.getString('cached_tickets');
+
+    if (cachedData != null) {
+      return (jsonDecode(cachedData) as List)
+          .map((e) => Ticket.fromJson(e))
+          .toList();
+    }
+
+    await Future.delayed(
+        const Duration(seconds: 2)); // Simula tempo de resposta
     final String response =
         await rootBundle.loadString('lib/assets/mocks/tickets.json');
     final List<dynamic> data = jsonDecode(response);
+
+    // Salva no cache
+    prefs.setString('cached_tickets', jsonEncode(data));
 
     return data.map((e) => Ticket.fromJson(e)).toList();
   }
