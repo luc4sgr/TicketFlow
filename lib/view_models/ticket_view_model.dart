62801d539ef3c02d69fd6155a9699a95d076f6c9
luc4sgr <23.lucasdoliveira@gmail.com>
2025-01-31 15:02:29 -0300

    add TicketViewModel to manage ticket loading and state

---
+++ lib/view_models/ticket_view_model.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import '../services/ticket_service.dart';
+import '../models/ticket.dart';
+
+class TicketViewModel extends ChangeNotifier {
+  final TicketService _ticketService = TicketService();
+  List<Ticket> _tickets = [];
+  bool _isLoading = false;
+
+  List<Ticket> get tickets => _tickets;
+  bool get isLoading => _isLoading;
+
+  Future<void> loadTickets() async {
+    _isLoading = true;
+    notifyListeners();
+
+    try {
+      _tickets = await _ticketService.fetchTicket();
+    } catch (e) {
+      _tickets = [];
+    }
+    _isLoading = false;
+    notifyListeners();
+  }
+}
