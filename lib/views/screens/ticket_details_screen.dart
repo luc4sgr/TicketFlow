103821a67c22c8a5a862be19f0480558b5cd987d
luc4sgr <23.lucasdoliveira@gmail.com>
2025-01-31 16:00:21 -0300

    enhance TicketDetailsScreen to display ticket information using TicketViewModel

--- lib/views/screens/ticket_details_screen.dart
+++ lib/views/screens/ticket_details_screen.dart
@@ -1,17 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../../view_models/ticket_view_model.dart';
+import '../../models/ticket.dart';
 
 class TicketDetailsScreen extends StatelessWidget {
   final String ticketId;
+
   const TicketDetailsScreen({super.key, required this.ticketId});
 
   @override
   Widget build(BuildContext context) {
+    final viewModel = Provider.of<TicketViewModel>(context);
+    final ticket = viewModel.tickets.firstWhere(
+      (ticket) => ticket.id.toString() == ticketId,
+      orElse: () =>
+          Ticket(id: 0, title: "Não encontrado", description: "", status: ""),
+    );
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Detalhes doTicket #$ticketId'),
       ),
-      body: Center(
-        child: Text('Informações detalhadas do ticket #$ticketId'),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(ticket.title), //TODO:verificar o theme
+            const SizedBox(height: 10),
+            Text(ticket.description),
+            const SizedBox(height: 10),
+            Text(
+              'Status: $ticket.status',
+              style: const TextStyle(fontWeight: FontWeight.bold),
+            ),
+          ],
+        ),
       ),
     );
   }
